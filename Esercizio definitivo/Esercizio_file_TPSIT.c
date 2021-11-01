027175089771eaf4b790243483de031b9a3ee5d6
Francescodl04 <72445602+Francescodl04@users.noreply.github.com>
2021-11-01 16:12:12 +0100

    Completamento esercizi

--- Esercizio definitivo/Esercizio_file_TPSIT.c
+++ Esercizio definitivo/Esercizio_file_TPSIT.c
@@ -1,50 +1,100 @@
+/** @author Francesco Di Lena
+ * @file Esercizio File TPSIT.c
+ * @version 1.0
+ * @date 01/11/2021
+ **/
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-main(int argc, char *argv[])
+int conta_parole(char contenuto_input[100][100], char parola_cercata[100], int contatore, int numero_parole) //Funzione che permette di contare il numero di parole corrispondenti a quella inserita all'interno di più stringhe.
 {
-	printf("Benvenuto nel programma!\n");
-	if(argc == 5)
+	int i;
+	for (i = 0; i < numero_parole; i++)
 	{
-    	FILE *file_input, *file_output;
-    	char *contenuto_input[0], *contenuto_output, *tipo_ricerca, *contenuto_ricerca;
-    	file_input = fopen(argv[1], "r"); //Apre il file di input in sola lettura.
-    	file_output = fopen(argv[2], "w"); //Apre il file di output in sola scrittura.
-    	tipo_ricerca = argv[3];
-    	contenuto_ricerca = argv[4];
-		if(tipo_ricerca == "-c")
+		if (strcmp(contenuto_input[i], parola_cercata) == 0) //Con strcmp si effettua il confronto tra due stringhe.
 		{
-			int i;
-			for(i = 0; i < 10; i= i + 1)
+			contatore++;
+		}
+	}
+	return contatore;
+}
+
+int conta_caratteri(char contenuto_input[100][100], char *carattere_cercato, int contatore, int numero_parole) //Funzione che permette di contare il numero di caratteri corrispondenti a quello inserito all'interno di più stringhe.
+{
+	int i, j;
+	for (i = 0; i < numero_parole; i++)
+	{
+		for (j = 0; j < strlen(contenuto_input[i]); j++) // strlen identifica la lunghezza di una stringa.
+		{
+			if (contenuto_input[i][j] == *carattere_cercato)
 			{
-				if(contenuto_input == contenuto_ricerca)
-				{
-					
-				}
+				contatore++;
 			}
 		}
-		else if(tipo_ricerca == "-p")
+	}
+	return contatore;
+}
+
+void stampa_output(char percorso_file [200], char contenuto_output[100][100]) //Funzione che permette di stampare su file una stringa.
+{
+	FILE *file_output;
+	file_output=fopen(percorso_file, "w"); // fopen permette di aprire un file (in scrittura). Qui il valore che restituisce viene assegnato ad un puntatore(file_output).
+	fprintf(file_output, "%s", contenuto_output); //Il metodo fprintf serve per stampare su file una o più stringhe.
+	fclose(file_output); // fclose chiude un file.
+}
+
+int main(int argc, char *argv[]) //Funzione principale richiamata dal programma ad ogni suo avvio.
+{
+	printf("\nBenvenuto nel programma!\n");
+	if (argc == 5)
+	{
+		FILE *file_input, *file_output;
+		file_input = fopen(argv[1], "r");
+		file_output = fopen(argv[2], "r");
+		fclose(file_output);
+		if (file_input == NULL || file_output == NULL)
 		{
-			int lunghezza_array = 0;
-			while(!feof(file_input))
-    		{
-    			contenuto_input = realloc(contenuto_input, lunghezza_array + 1 * sizeof(int));
-				fscanf(file_input, "%s", &contenuto_input); //Legge il contenuto del file.
-			}
-			int i;
-			for(i = 0; i < 10; i= i + 1)
+			printf("\nI percorsi dei file specificati non esistono.");
+		}
+		else if (argv[3][1] != 'c' && argv[3][1] != 'p')
+		{
+			printf("\nNon hai inserito dei comandi giusti per effettuare la ricerca di caratteri o parole.");
+		}
+		else
+		{
+			char contenuto_input[100][100], contenuto_output[100][100];
+			int numero_parole = 0;
+			int contatore = 0;
+			while (!feof(file_input))
 			{
-				if(contenuto_input == contenuto_ricerca)
-				{
-					
-				}
+				fscanf(file_input, "%s\t", contenuto_input[numero_parole]); // fscanf permette di leggere il contenuto di un file.
+				numero_parole++;
+			}
+			switch (argv[3][1])
+			{
+			case 'c':
+				contatore = conta_caratteri(contenuto_input, argv[4], contatore, numero_parole); 
+				sprintf(*contenuto_output, "Nel file di input il carattere \"%s\" è stato contato %d volte.", argv[4], contatore); //sprintf permette di concatenare più stringhe per inserirle in una sola.
+				stampa_output(argv[2], contenuto_output);
+				printf("\nHai scelto la modalita' di ricerca a carattere, inserendo il carattere \"%s\".\nIl numero di caratteri ricercati verra' stampato nel file indicato nell'invocazione.", argv[4]);
+				break;
+
+			case 'p':
+				contatore = conta_parole(contenuto_input, argv[4], contatore, numero_parole);
+				sprintf(*contenuto_output, "Nel file di input la parola \"%s\" è stata contata %d volte.", argv[4], contatore); //sprintf permette di concatenare più stringhe per inserirle in una sola.
+				stampa_output(argv[2], contenuto_output);
+				printf("\nHai scelto la modalita' di ricerca a carattere, inserendo la parola \"%s\".\nIl numero di parole ricercate verra' stampato nel file indicato nell'invocazione.", argv[4]);
+				break;
 			}
 		}
-		fclose(file_input); //Chiude il file di input.
-    	fclose(file_output); //Chiude il file di output.	
 	}
 	else
 	{
-		printf("Non hai inserito abbastanza argomenti all'invocazione del programma, riprova...\n");
+		printf("\nNon hai inserito abbastanza argomenti all'invocazione del programma, riprova di nuovo...\n");
 	}
+	fflush(stdin); //Permette di ricevere un altro input da tastiera.
+	printf("\nPer uscire dal programma, premere un tasto qualsiasi...");
+	getchar();
 }
--- Esercizio di prova/esercizio_prova.c
+++ Esercizio di prova/esercizio_prova.c
@@ -3,97 +3,92 @@
 * @date 28/10/2021
 */
 
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main() 
+int conta_caratteri(char contenuto_input[100][100], int numero_parole, int caratteri_parole_trovati, char carattere_ricercato)
 {
-  	printf("Benvenuto nel programma!\n");
-  	FILE *file_input, *file_output; //Viene dichiarato il file.
-  	char contenuto_input; //Stringa che conterrà ogni parola del file.
-  	file_input = fopen("input.txt", "r"); //Apre il file esclusivamente in lettura.
-  	if( file_input == NULL ) 
-  	{
-    	perror("Errore in apertura del file");
-    	exit(1);
-  	}
-  	printf("\nPer cercare un carattere inserisci -c, mentre per cercare una stringa inserisci -p:\n");
-  	char scelta[1][2];
-  	scanf("%s", scelta);
-  	if(strcmp(scelta[0], "-c") == 0)
-  	{
-  		printf("\nInserisci il carattere che vuoi ricercare all'interno del file:\n");
-  		char carattere_ricercato;
-  		int contatore=0;
-  		scanf(" %c", &carattere_ricercato);
-  		printf("\n");
-  		do //Legge e stampa a schermo ogni riga.
-		{
-    		contenuto_input = fgetc(file_input);
-    		printf("%c", contenuto_input);
-			if(contenuto_input==carattere_ricercato)
-			{
-				contatore++;
-			}
-  		}
-  		while(contenuto_input!=EOF);
-		printf("\n\n\nIl carattere \"%c\"e' stato trovato %d volte.\n", carattere_ricercato, contatore);
-	}
-	else if (strcmp(scelta[0], "-p") == 0)
+	int num = 0;
+	int i, j;
+	for (i = 0; i < numero_parole; i++)
 	{
-		printf("\nInserisci la stringa che vuoi ricercare all'interno del file:\n");
-  		char carattere_ricercato[50];
-  		int contatore = 0, i, lunghezza_parola;
-  		scanf("%s", &carattere_ricercato);
-  		printf("\n");
-		do
+		for (j = 0; j < strlen(contenuto_input[i]); j++)
 		{
-			contenuto_input = fgetc(file_input);
-			printf("%c", contenuto_input);
-			if( contenuto_input=='.' || contenuto_input==',' || contenuto_input==';' || contenuto_input==':')
+			if (contenuto_input[i][j] == carattere_ricercato)
 			{
-				i++;
+				caratteri_parole_trovati++;
 			}
-			else
-			{
-				if(contenuto_input == ' ' || contenuto_input == '\n')
-				{
-					if(lunghezza_parola == i)
-					{
-						contatore++;
-					}
-					i = 0;
-					lunghezza_parola=0;
-				}
-				else if(contenuto_input == carattere_ricercato[i])
-				{
-					lunghezza_parola++;
-					i++;
-				}
-				else
-				{
-					i++;
-				}
-			}
-			
 		}
-  		while(contenuto_input!=EOF);
-		printf("\n\n\nLa parola \"%s\" e' stata trovata %d volte.\n", carattere_ricercato, contatore);
 	}
-	printf("\nVuoi salvare i risultati all'interno di un file? (Inserisci S per Si, oppure N per No)");
-  	fflush(stdin);
-  	char conferma;
-  	scanf("%c", &conferma);
-  	if(conferma=='S')
-  	{
-		printf("\nSalvataggio completato.");
-  		fclose(file_input); //Chiude il file.
-  		fclose(file_output); //Chiude il file.
-	}
-	printf("\nPer uscire dal programma premi un tasto qualsiasi...");
-  	fflush(stdin);
-  	getchar();
-  	return 0;
+	return caratteri_parole_trovati;
 }
 
+int conta_parole(char contenuto_input[100][100], int numero_parole, int caratteri_parole_trovati, char parola_ricercata[100])
+{
+	int num = 0;
+	int i;
+	for (i = 0; i < numero_parole; i++)
+	{
+		if (strcmp(contenuto_input[i], parola_ricercata) == 0)
+		{
+			caratteri_parole_trovati++;
+		}
+	}
+	return caratteri_parole_trovati;
+}
+
+void scrivi_output(char contenuto_output[100][100])
+{
+	FILE *file_output;
+	file_output = fopen("output.txt", "w");
+	fprintf(file_output, "%s", contenuto_output);
+}
+
+int main(int argc, char *argv[])
+{
+	FILE *file_input;
+	char scelta[3];
+	char contenuto_input[100][100], contenuto_output[100][100];
+	int caratteri_parole_trovati = 0;
+	printf("\nBenvenuto nel programma!\n");
+	file_input = fopen("input.txt", "r");
+	if (file_input == NULL)
+	{
+		printf("\nIl file di input non esiste, pertanto non Ã¨ possibile eseguire operazioni.\n");
+		exit(1);
+	}
+	int numero_parole = 0;
+	while (!feof(file_input))
+	{
+		fscanf(file_input, "%s\t", contenuto_input[numero_parole]);
+		numero_parole++;
+	}
+	printf("\nVuoi ricercare una parola oppure un carattere?\n");
+	scanf("%s", scelta);
+	fflush(stdin);
+	if (scelta[1] == 'p')
+	{
+		printf("\nInserisci la parola che desideri ricercare:\n");
+		char parola_ricercata[100];
+		scanf("%s", &parola_ricercata);
+		caratteri_parole_trovati = conta_parole(contenuto_input, numero_parole, caratteri_parole_trovati, parola_ricercata);
+		sprintf(*contenuto_output, "La parola \"%s\" compare %d volte.", parola_ricercata, caratteri_parole_trovati);
+		scrivi_output(contenuto_output);
+	}
+	else if (scelta[1] == 'c')
+	{
+		printf("\nInserisci il carattere che desideri ricercare:\n");
+		char carattere_ricercato;
+		scanf("%s", &carattere_ricercato);
+		caratteri_parole_trovati = conta_caratteri(contenuto_input, numero_parole, caratteri_parole_trovati, carattere_ricercato);
+		sprintf(*contenuto_output, "Il carattere \"%c\" compare %d volte.", carattere_ricercato, caratteri_parole_trovati);
+		scrivi_output(contenuto_output);
+	}
+	fclose(file_input);
+	fflush(stdin);
+	printf("\nIl risultato e' stato scritto in output.txt. Per uscire dal programma premi un tasto qualsiasi...");
+	getchar();
+	exit(0);
+	return 0;
+}
